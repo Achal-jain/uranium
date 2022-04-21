a3f43ba5774f6128d2c8d08ad40ef3fb5f87dfcf
Achal-jain <achaljain412@gmail.com>
2022-04-22 00:16:10 +0530

    comp

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,11 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
-  let data = abcd.body;
+const createUser = async function (req, res) {
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
-};
+  res.send({ msg: savedData })
+}
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
@@ -31,10 +27,10 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FUnctionUp",
+      work: "Actor",
+      organisation: "Bollywood",
     },
-    "functionup-thorium"
+    "Bollywood-Actor"
   );
   res.setHeader("x-auth-token", token);
   res.send({ status: true, data: token });
@@ -54,7 +50,7 @@ const getUserData = async function (req, res) {
   // Input 1 is the token to be decoded
   // Input 2 is the same secret with which the token was generated
   // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-thorium");
+  let decodedToken = jwt.verify(token, "Bollywood-Actor");
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
 
@@ -81,10 +77,18 @@ const updateUser = async function (req, res) {
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  res.send({ status: updatedUser});
 };
+
+const deleteUser=async function(req,res){
+let userId =req.params.userId
+let deleteData =await userModel.findOneAndUpdate({_id:userId},{$set:{isDeleted:true}},{new:true}
+  )
+res.send({status:true, data:deleteData})
+}
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
